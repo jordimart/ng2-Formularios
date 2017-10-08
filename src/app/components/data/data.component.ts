2e8ee837ce6e4e174f8d5d8478b3ffa35c7f1c95
Jordi Martínez Frias <jordimart83@gmail.com>
2017-10-08 12:37:30 +0200

    [ADD] Agrupaciones de los objetos - formGroupNombre

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -9,15 +9,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 export class DataComponent {
 
   forma: FormGroup;
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: "Jorge",
+      apellido: "Martínez"
+    },
+    email: "jordimart83@gmail.com"
+  }
 
   constructor() {
 
     this.forma = new FormGroup({
-      'nombre': new FormControl('', [
-        Validators.required,
-        Validators.minLength(3)
-      ]),
-      'apellido': new FormControl('', Validators.required),
+      nombrecompleto: new FormGroup({
+        'nombre': new FormControl('', [
+          Validators.required,
+          Validators.minLength(3)
+        ]),
+        'apellido': new FormControl('', Validators.required)
+      }),
       'email': new FormControl('', [
         Validators.required,
         Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
